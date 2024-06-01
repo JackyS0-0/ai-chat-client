93a309babc9731255ab026512850347f0c8f85ec
JackyS0-0 <949453242@qq.com>
2024-06-01 09:39:46 +0800

    update readme.md

--- src/Apis.jsx
+++ src/Apis.jsx
@@ -5,7 +5,10 @@ const configs = {
     api_key: 'xxx',
 }
 
-//此处不介意直接在网页客户端中直接发送请求（因为请求头中填写的是你的api_key）
+//It is not recommended to send requests directly from the web client (because the API key is included in the request headers).
+//此处不建议直接在网页客户端中直接发送请求（因为请求头中填写的是你的api_key）
+
+//It is recommended to start a backend service (such as a Node.js server). The client can then communicate with the server, which will in turn make the HTTP requests.
 //建议启动一个后端服务(如Node服务) 由客户端访问服务端，再由服务端来发起http请求 
 const Apis = {
     chatgpt: async ({ messages: reqMessages }) => {
@@ -34,6 +37,5 @@ const Apis = {
         }
     }
 }
-
 
 export default Apis;
